69bde61562b0b349f53f5f26f76e4c4ea7409eb4
Nathaniel538 <83531199+Nathaniel538@users.noreply.github.com>
2021-11-13 12:16:23 +0530

    Add files via upload

---
+++ job_description.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class JobDescription extends StatefulWidget {
+  const JobDescription({Key? key}) : super(key: key);
+
+  @override
+  _JobDescriptionState createState() => _JobDescriptionState();
+}
+
+class _JobDescriptionState extends State<JobDescription> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("UC"),
+    );
+  }
+}
